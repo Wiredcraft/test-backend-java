853f7a1af7c5db76a252b1cb6b1c98ac6346c0d7
zhangyongxin <zhangyongxin@ncarzone.com>
2022-03-20 13:39:53 +0800

    初始化sql提供

--- work-demo/README.md
+++ work-demo/README.md
@@ -1,7 +1,8 @@
 * 使用java8
 * 使用lombok插件
-* 使用ehcache
+* 缓存使用ehcache
 * 刷入mysql.sql
+* 启动工程 run com.zhangyongxin.demo.Application
 * api文档：`http://127.0.0.1:8080/demo/swagger-ui.html`
 * helloWorld : `http://127.0.0.1:8080/demo/test/hello`
 * 所有数据接口均返回Result，如：
@@ -15,5 +16,5 @@
     ```
 
 * 分页使用PageHelper
-* 权限控制使用spring security，开发环境下不做登录校验
-* 代码生成：Run -> Edit Configurations -> + Maven commend line ```mybatis-generator:generate -e```
+* 权限控制使用spring security
+* dao代码生成：Run -> Edit Configurations -> + Maven commend line ```mybatis-generator:generate -e```
--- work-demo/src/main/resources/mysql.sql
+++ work-demo/src/main/resources/mysql.sql
@@ -1,16 +1,34 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : 127.0.0.1
+ Source Server Type    : MySQL
+ Source Server Version : 50713
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : zhangyx_test
+
+ Target Server Type    : MySQL
+ Target Server Version : 50713
+ File Encoding         : 65001
+
+ Date: 20/03/2022 13:38:48
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for role_user
 -- ----------------------------
 DROP TABLE IF EXISTS `role_user`;
 CREATE TABLE `role_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  KEY `user_id_idx` (`user_id`),
-  KEY `role_id_idx` (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;;
+                             `id` int(11) NOT NULL AUTO_INCREMENT,
+                             `user_id` int(11) DEFAULT NULL,
+                             `role_id` int(11) NOT NULL,
+                             PRIMARY KEY (`id`) USING BTREE,
+                             KEY `user_id_idx` (`user_id`),
+                             KEY `role_id_idx` (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
 -- Records of role_user
@@ -24,19 +42,19 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_menu`;
 CREATE TABLE `tb_menu` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `url` varchar(100) DEFAULT '' COMMENT 'url',
-  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '名称',
-  `remark` varchar(500) DEFAULT NULL COMMENT '角色备注',
-  `ico` varchar(100) DEFAULT '' COMMENT '图标',
-  `model` varchar(100) DEFAULT '' COMMENT '所属模块',
-  `p_id` bigint(20) unsigned DEFAULT NULL COMMENT '父节点',
-  `is_delete` int(1) DEFAULT '0' COMMENT '逻辑删除，0表示不删除',
-  `sort` int(12) DEFAULT '0' COMMENT '排序',
-  `permission` varchar(250) NOT NULL COMMENT '符合Shiro格式的权限字符串，例如[maintain:*]',
-  `active_url` varchar(100) DEFAULT NULL COMMENT '选中url',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;;
+                           `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+                           `url` varchar(100) DEFAULT '' COMMENT 'url',
+                           `name` varchar(100) NOT NULL DEFAULT '' COMMENT '名称',
+                           `remark` varchar(500) DEFAULT NULL COMMENT '角色备注',
+                           `ico` varchar(100) DEFAULT '' COMMENT '图标',
+                           `model` varchar(100) DEFAULT '' COMMENT '所属模块',
+                           `p_id` bigint(20) unsigned DEFAULT NULL COMMENT '父节点',
+                           `is_delete` int(1) DEFAULT '0' COMMENT '逻辑删除，0表示不删除',
+                           `sort` int(12) DEFAULT '0' COMMENT '排序',
+                           `permission` varchar(250) NOT NULL COMMENT '符合Shiro格式的权限字符串，例如[maintain:*]',
+                           `active_url` varchar(100) DEFAULT NULL COMMENT '选中url',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_menu
@@ -51,17 +69,17 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_role`;
 CREATE TABLE `tb_role` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '名称',
-  `remark` varchar(500) DEFAULT NULL COMMENT '角色备注',
-  `create_date` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  `create_user` bigint(20) unsigned DEFAULT NULL COMMENT '创建人',
-  `update_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `update_user` bigint(20) unsigned DEFAULT NULL COMMENT '更新人',
-  `state` int(1) DEFAULT '1' COMMENT '是否冻结，0表示冻结，1表示正常',
-  `is_delete` int(1) DEFAULT '0' COMMENT '逻辑删除，0表示不删除',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;;
+                           `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                           `name` varchar(100) NOT NULL DEFAULT '' COMMENT '名称',
+                           `remark` varchar(500) DEFAULT NULL COMMENT '角色备注',
+                           `create_date` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+                           `create_user` bigint(20) unsigned DEFAULT NULL COMMENT '创建人',
+                           `update_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                           `update_user` bigint(20) unsigned DEFAULT NULL COMMENT '更新人',
+                           `state` int(1) DEFAULT '1' COMMENT '是否冻结，0表示冻结，1表示正常',
+                           `is_delete` int(1) DEFAULT '0' COMMENT '逻辑删除，0表示不删除',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_role
@@ -75,13 +93,13 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_role_menu`;
 CREATE TABLE `tb_role_menu` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `role_id` bigint(20) unsigned NOT NULL COMMENT '角色id',
-  `menu_id` bigint(20) unsigned NOT NULL COMMENT '菜单id',
-  PRIMARY KEY (`id`),
-  KEY `ROLE_ID_INDEX` (`role_id`),
-  KEY `MENU_ID_INDEX` (`menu_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='角色与菜单的关联表，负责权限管理';;
+                                `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+                                `role_id` bigint(20) unsigned NOT NULL COMMENT '角色id',
+                                `menu_id` bigint(20) unsigned NOT NULL COMMENT '菜单id',
+                                PRIMARY KEY (`id`),
+                                KEY `ROLE_ID_INDEX` (`role_id`),
+                                KEY `MENU_ID_INDEX` (`menu_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='角色与菜单的关联表，负责权限管理';
 
 -- ----------------------------
 -- Records of tb_role_menu
@@ -96,26 +114,24 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) NOT NULL DEFAULT '' COMMENT '用户名',
-  `password` varchar(32) DEFAULT NULL COMMENT '密码',
-  `usertype` varchar(2) DEFAULT NULL COMMENT '用户类型 1后台用户 2微信用户',
-  `enabled` int(2) DEFAULT NULL COMMENT '是否可用',
-  `realname` varchar(32) DEFAULT NULL COMMENT '真实姓名',
-  `qq` varchar(14) DEFAULT NULL COMMENT 'QQ',
-  `email` varchar(100) DEFAULT NULL,
-  `tel` varchar(255) DEFAULT NULL COMMENT '联系电话',
-  `head_img` varchar(255) DEFAULT NULL COMMENT '头像地址',
-  `version` int(11) DEFAULT NULL,
-  `creation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+                             `id` int(11) NOT NULL AUTO_INCREMENT,
+                             `username` varchar(32) NOT NULL DEFAULT '' COMMENT '用户名',
+                             `password` varchar(32) DEFAULT NULL COMMENT '密码',
+                             `name` varchar(32) NOT NULL DEFAULT '' COMMENT '姓名',
+                             `dob` date DEFAULT NULL COMMENT '生日',
+                             `address` varchar(255) DEFAULT NULL COMMENT '地址',
+                             `description` varchar(255) DEFAULT NULL COMMENT '描述',
+                             `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                             PRIMARY KEY (`id`),
+                             UNIQUE KEY `username` (`username`),
+                             KEY `idx_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户信息表';
 
 -- ----------------------------
 -- Records of user_info
 -- ----------------------------
 BEGIN;
-INSERT INTO `user_info` VALUES (1, 'admin', 'admin',1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-06-22 10:05:28');
+INSERT INTO `user_info` VALUES (1, 'admin', 'admin', '', NULL, NULL, NULL, '2022-03-20 13:38:33');
 COMMIT;
 
+SET FOREIGN_KEY_CHECKS = 1;

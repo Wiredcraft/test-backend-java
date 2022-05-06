6c3fa40426c7ab86ad055b53330aa406a4d53d5d
Jerry Chin <metrxqin@gmail.com>
2022-05-06 21:44:53 +0800

    Import files generated by Spring Initializer.

---
+++ src/test/java/io/github/jerrychin/testbackendjava/TestBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jerrychin.testbackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

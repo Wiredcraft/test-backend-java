730145d38c5f8e8068ee7f1af3968a722a8ec51d
Jeremy.zhang <jeresuxing@163.com>
2022-10-19 11:05:13 +0800

    support database config & add jpa frame

---
+++ wiredcraft-user/src/main/java/com/wiredcraft/user/Application.java
@@ -0,0 +1,15 @@
+package com.wiredcraft.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jeremy.zhang
+ * @date 2022-10-19
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

f75e10f777e78851372bd966ac68b00b65c51b1e
Jeremy.zhang <jeresuxing@163.com>
2022-10-20 13:50:46 +0800

    add swagger annotation

--- wiredcraft-user/src/main/java/com/wiredcraft/user/Application.java
+++ wiredcraft-user/src/main/java/com/wiredcraft/user/Application.java
@@ -2,11 +2,13 @@ package com.wiredcraft.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author jeremy.zhang
  * @date 2022-10-19
  */
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
